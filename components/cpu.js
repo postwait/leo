0146100ef66d3c282942e72f8b373843ea17ab17
Corey Cossentino <corey@omniti.com>
2015-02-12 17:05:24 -0500

    more work on the program's structure and the basic layout of each component

---
+++ components/cpu.js
@@ -0,0 +1,15 @@
+function initialize(info, callback) {
+    callback(null);
+}
+
+function main() {
+}
+
+function cleanup() {
+}
+
+module.exports = {
+    initialize: initialize,
+    main: main,
+    cleanup: cleanup
+};
