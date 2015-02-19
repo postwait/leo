a73562bcaa5d21e02c1191051fac0fbb19bea2ed
Corey Cossentino <corey@omniti.com>
2015-02-19 16:26:41 -0500

    genericize adding metrics from script output

--- components/cpu.js
+++ components/cpu.js
@@ -1,34 +1,10 @@
-var spawn = require('child_process').spawn;
 var path = require('path');
-var util = require('util');
 
 module.exports = function CPU() {
     this.initialize = function(callback) {
         var scriptPath = path.join(this.userConfig.nadpath, "etc", "node-agent.d", "cpu.sh");
-        var script = spawn(scriptPath);
-        this.buffer = "";
-        this.metricNames = [];
+        this.addMetricsFromScriptOutput(scriptPath, callback);
 
-        script.stdout.on('data', function(data) {
-            this.buffer = this.buffer + data;
-            var lines = this.buffer.split(/\r?\n/);
-            this.buffer = lines.pop();
-
-            lines.forEach(function(line) {
-                var lineVars = line.split(/\s+/);
-                this.metricNames.push(lineVars[0]);
-            }.bind(this));
-        }.bind(this));
-
-        script.stdout.on('close', function(code) {
-            if(code != 0) {
-                return callback(util.format("%s exited with status %s", scriptPath, code));
-            }
-
-            return callback();
-        });
-
-        script.stdin.end();
     };
 
     this.main = function(callback) {
