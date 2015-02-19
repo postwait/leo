6b0ee228f198ac156eb34edb08ede2481151f280
Corey Cossentino <corey@omniti.com>
2015-02-19 12:01:24 -0500

    make components objects

--- components/cpu.js
+++ components/cpu.js
@@ -1,56 +1,52 @@
-exports.initialize = function(info, callback) {
-    callback(null);
-};
-
-exports.prompts = function(userConfig, configData, callback) {
-    callback(null);
-};
-
-exports.main = function(userConfig, configData, callback) {
-    configData.graphs["CPU"] = {
-        "style": "line",
-        "datapoints": [
-            {
-                "name": "CPU Kernel",
-                "metric_name": "aggcpu`cpu_stat:all:sys:cpu_kernel",
-                "metric_type": "numeric",
-                "axis": "l",
-                "stack": 0,
-                "derive": "counter",
-                "hidden": false
-            },
-            {
-                "name": "CPU User",
-                "metric_name": "aggcpu`cpu_stat:all:sys:cpu_user",
-                "metric_type": "numeric",
-                "axis": "l",
-                "stack": 0,
-                "derive": "counter",
-                "hidden": false
-            },
-            {
-                "name": "CPU Idle",
-                "metric_name": "aggcpu`cpu_stat:all:sys:cpu_idle",
-                "metric_type": "numeric",
-                "axis": "l",
-                "stack": 0,
-                "derive": "counter",
-                "hidden": false
-            }
-        ]
+module.exports = function CPU() {
+    this.initialize = function(callback) {
+        return callback();
     };
 
-    configData.check.metrics.numeric.push(
-        "aggcpu`cpu_stat:all:sys:cpu_idle",
-        "aggcpu`cpu_stat:all:sys:cpu_user",
-        "aggcpu`cpu_stat:all:sys:cpu_kernel",
-        "aggcpu`cpu_stat:all:sys:sysfork",
-        "aggcpu`cpu_stat:all:sys:syscall"
-    );
+    this.prompts = function(callback) {
+        return callback();
+    };
 
-    callback(null);
+    this.main = function(callback) {
+        this.addMetric("aggcpu`cpu_stat:all:sys:cpu_idle");
+        this.addMetric("aggcpu`cpu_stat:all:sys:cpu_user");
+        this.addMetric("aggcpu`cpu_stat:all:sys:cpu_kernel");
+        this.addMetric("aggcpu`cpu_stat:all:sys:sysfork");
+        this.addMetric("aggcpu`cpu_stat:all:sys:syscall");
+
+        this.addGraph("CPU", {
+            "style": "line",
+            "datapoints": [
+                {
+                    "name": "CPU Kernel",
+                    "metric_name": "aggcpu`cpu_stat:all:sys:cpu_kernel",
+                    "metric_type": "numeric",
+                    "axis": "l",
+                    "stack": 0,
+                    "derive": "counter",
+                    "hidden": false
+                },
+                {
+                    "name": "CPU User",
+                    "metric_name": "aggcpu`cpu_stat:all:sys:cpu_user",
+                    "metric_type": "numeric",
+                    "axis": "l",
+                    "stack": 0,
+                    "derive": "counter",
+                    "hidden": false
+                },
+                {
+                    "name": "CPU Idle",
+                    "metric_name": "aggcpu`cpu_stat:all:sys:cpu_idle",
+                    "metric_type": "numeric",
+                    "axis": "l",
+                    "stack": 0,
+                    "derive": "counter",
+                    "hidden": false
+                }
+            ]
+        });
+
+        return callback();
+    }
 };
-
-exports.cleanup = function(userConfig, configData, callback) {
-};
-
