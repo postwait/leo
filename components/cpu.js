18281176adc60b979da92697322e2b2505b7b9e6
Corey Cossentino <corey@omniti.com>
2015-02-23 16:42:22 -0500

    create default graphs

--- components/cpu.js
+++ components/cpu.js
@@ -3,4 +3,15 @@ var path = require('path');
 module.exports = function CPU() {
     this.nadScriptFilename = "cpu.sh";
     this.metricPrefix = "cpu";
+
+    this.defaultMetrics = ["kernel", "user"];
+    this.defaultGraphs = [
+        {
+            "title": "CPU Usage",
+            "datapoints": [
+                { "metric_name": "cpu`kernel", "name": "Kernel" },
+                { "metric_name": "cpu`user",   "name": "User" }
+            ]
+        }
+    ];
 };
